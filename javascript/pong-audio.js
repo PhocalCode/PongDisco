738ac2a756fe48a9620a3e54aa7a9d5e3a40623c
PhocalCode <150416659+PhocalCode@users.noreply.github.com>
2025-01-03 16:34:01 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("Paddle.mp4");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("silence.mp3");
+export var scoreSound = new soundFile("Wall.mp4");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("PongMusic.mp4");
